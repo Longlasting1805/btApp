1e3f1470b513e31c260c463631dc49152528f327
Longlasting1805 <Longlasting1805@gmail.com>
2025-02-03 18:14:43 +0100

    Cover page removed

--- Desktop/btApp/bt-application/src/App.js
+++ Desktop/btApp/bt-application/src/App.js
@@ -1,21 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
-import Proceed from './Components/Proceed/Proceed';
 // import Proceed from './Components/Proceed/Proceed';
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
+// import Proceed from './Components/Proceed/Proceed';
+// import {Routes, Route, BrowserRouter} from 'react-router-dom'
 
 
 function App() {
   return (
     <div >
-      <BrowserRouter>
+       {/* <BrowserRouter>
         <Routes>
           <Route index element={<Proceed />} />
           <Route path="/Login" element={<LoginSignup />} />
         </Routes>
-      </BrowserRouter>
-    
+      </BrowserRouter> */}
+      <LoginSignup />
+     
    
     </div>
   );
